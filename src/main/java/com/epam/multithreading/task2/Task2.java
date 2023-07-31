dc0352657b8cc6a010a2af34daa8992678fa8006
Anzhalika Baradzina <anzhalika_baradzina@epam.com>
2023-07-31 13:51:26 +0200

    updated task2

--- src/main/java/com/epam/multithreading/task2/Task2.java
+++ src/main/java/com/epam/multithreading/task2/Task2.java
@@ -3,14 +3,18 @@ package com.epam.multithreading.task2;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+
 public class Task2 {
 
     public static void main(String[] args) throws InterruptedException {
         List<Integer> list = new ArrayList<>();
         Random random = new Random();
-        Thread addThread = new Thread(new AddElementsIntoTheMap(list, random));
-        Thread sumThread = new Thread(new SumNumbers(list));
-        Thread squareThread = new Thread(new Square(list));
+        Object lock = new Object();
+
+        Thread addThread = new Thread(new AddElementsIntoTheMap(list, random, lock));
+        Thread sumThread = new Thread(new SumNumbers(list, lock));
+        Thread squareThread = new Thread(new Square(list, lock));
+
         addThread.start();
         sumThread.start();
         squareThread.start();
@@ -21,25 +25,29 @@ public class Task2 {
 }
 
 class AddElementsIntoTheMap implements Runnable {
-    private List list;
+    private List<Integer> list;
     private Random random;
+    private Object lock;
 
-    public AddElementsIntoTheMap(List list, Random random) {
+    public AddElementsIntoTheMap(List<Integer> list, Random random, Object lock) {
         this.list = list;
         this.random = random;
+        this.lock = lock;
     }
 
-    public synchronized void add(List<Integer> list, Random random) throws InterruptedException {
-        list.add(random.nextInt((9)));
-        System.out.println("writing random number " + random.nextInt((9)));
-        Thread.sleep(1000);
+    public void add() throws InterruptedException {
+        synchronized (lock) {
+            list.add(random.nextInt(9));
+            System.out.println("writing random number " + list.get(list.size() - 1));
+            Thread.sleep(1000);
+        }
     }
 
     @Override
     public void run() {
         while (true) {
             try {
-                add(list, random);
+                add();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -48,22 +56,27 @@ class AddElementsIntoTheMap implements Runnable {
 }
 
 class SumNumbers implements Runnable {
-    private List list;
+    private List<Integer> list;
+    private Object lock;
 
-    public SumNumbers(List list) {
+    public SumNumbers(List<Integer> list, Object lock) {
         this.list = list;
+        this.lock = lock;
     }
 
-    public synchronized void printSum(List<Integer> list) throws InterruptedException {
-        System.out.println("printing sum of the numbers " + list.stream().reduce(0, Integer::sum));
-        Thread.sleep(5000);
+    public void printSum() throws InterruptedException {
+        synchronized (lock) {
+            int sum = list.stream().reduce(0, Integer::sum);
+            System.out.println("printing sum of the numbers " + sum);
+            Thread.sleep(5000);
+        }
     }
 
     @Override
     public void run() {
         while (true) {
             try {
-                printSum(list);
+                printSum();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -72,22 +85,28 @@ class SumNumbers implements Runnable {
 }
 
 class Square implements Runnable {
-    private List list;
+    private List<Integer> list;
+    private Object lock;
 
-    public Square(List list) {
+    public Square(List<Integer> list, Object lock) {
         this.list = list;
+        this.lock = lock;
     }
 
-    public synchronized void printSquare (List<Integer> list) throws InterruptedException {
-        System.out.println("printing square root of sum of squares of all numbers " + Math.sqrt(list.stream().reduce(0, Integer::sum)));
-        Thread.sleep(10000);
+    public void printSquare() throws InterruptedException {
+        synchronized (lock) {
+            int sumOfSquares = list.stream().map(x -> x * x).reduce(0, Integer::sum);
+            double squareRoot = Math.sqrt(sumOfSquares);
+            System.out.println("printing square root of sum of squares of all numbers " + squareRoot);
+            Thread.sleep(10000);
+        }
     }
 
     @Override
     public void run() {
         while (true) {
             try {
-                printSquare(list);
+                printSquare();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
